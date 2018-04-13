8988f614cb098446112309b5811a8264ff88b13c
Santiago de Pedro <santiago.pedro23@gmail.com>
2018-04-13 19:00:56 -0400

    Refactoring
    #5 | Change function namespaces
    #4 | Documentation of README.md

--- src/clj_google/core.clj
+++ src/clj_google/core.clj
@@ -1,12 +1,7 @@
 (ns clj-google.core
-  (:require [clj-http.client :as client]
-            [clojure.data.json :as json]))
-
+  (:require [clj-http.client :as client]))
 
 (defn content
   [url access-token]
   (client/get url {:headers {:authorization (str "Bearer " (get access-token :access_token))}}))
 
-(defn json
-  [data]
-  (json/read-str data :key-fn keyword))
