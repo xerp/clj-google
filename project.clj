c043a9644a90f25c7f14ace6b3b5b2e4dc2fb891
Santiago de Pedro <santiago.pedro23@gmail.com>
2018-04-13 11:24:04 -0400

    #1 | Integration with circleci

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/xerp/clj-google"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-http "3.8.0"]
                  [clj-oauth "1.5.5"]
                  [clj-json "0.5.3"]
