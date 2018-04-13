e555aeee618d205dee4a47f649acee1668cee205
Santiago de Pedro <santiago.pedro23@gmail.com>
2018-04-13 11:39:22 -0400

    #1 | workflow configuration #3 | add release conf at project.clj

--- project.clj
+++ project.clj
@@ -2,9 +2,13 @@
   :description "Google APIs Toolbox"
   :url "https://github.com/xerp/clj-google"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-http "3.8.0"]
                  [clj-oauth "1.5.5"]
                  [clj-json "0.5.3"]
-                 [ring/ring-codec "1.1.0"]])
+                 [ring/ring-codec "1.1.0"]]
+  :repositories [["releases" {:url           "https://clojars.org/repo"
+                              :username      :env/CLOJAR_USERNAME
+                              :password      :env/CLOJAR_PASSWORD
+                              :sign-releases false}]])
